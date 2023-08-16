29cda69df56e6bc314ae7bbd04f0a220717f8239
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-08-15 22:20:15 -0500

    Proyecto Java Iniciado - Farmacia

---
+++ src/farmacia/Farmacia.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package farmacia;
+
+/**
+ *
+ * @author bryan
+ */
+public class Farmacia {
+    
+    public static void main (String[] args){
+        
+    }
+    
+}
