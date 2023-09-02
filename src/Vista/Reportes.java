d5b49021b2203e837be5c892a79dc343bbc68d32
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-09-01 22:43:45 -0500

    bug fixed reporttes

--- src/Vista/Reportes.java
+++ src/Vista/Reportes.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package Vista;
-
+import java.sql.*;
 /**
  *
  * @author bryan
@@ -13,6 +13,12 @@ public class Reportes extends javax.swing.JFrame {
     /**
      * Creates new form Reportes
      */
+    
+    public static final String DB_URL = "jdbc:mysql://localhost/esfot-care";
+    public static final String USER = "root";
+    public static final String PASSWORD = "root2023";
+
+    
     public Reportes() {
         initComponents();
         this.setLocationRelativeTo(null);
@@ -30,7 +36,7 @@ public class Reportes extends javax.swing.JFrame {
 
         jLabel5 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        cajeroTable = new javax.swing.JTable();
         fechaBusInput = new javax.swing.JTextField();
         jButtonBuscarCajeroCod = new javax.swing.JButton();
         reporteCajeroBtn = new javax.swing.JButton();
@@ -60,7 +66,7 @@ public class Reportes extends javax.swing.JFrame {
         jLabel5.setText("Buscar por fecha:");
         getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, -1, -1));
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        cajeroTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null, null, null},
                 {null, null, null, null, null, null, null},
@@ -86,12 +92,12 @@ public class Reportes extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        jScrollPane1.setViewportView(jTable1);
-        if (jTable1.getColumnModel().getColumnCount() > 0) {
-            jTable1.getColumnModel().getColumn(0).setResizable(false);
-            jTable1.getColumnModel().getColumn(1).setResizable(false);
-            jTable1.getColumnModel().getColumn(2).setResizable(false);
-            jTable1.getColumnModel().getColumn(3).setResizable(false);
+        jScrollPane1.setViewportView(cajeroTable);
+        if (cajeroTable.getColumnModel().getColumnCount() > 0) {
+            cajeroTable.getColumnModel().getColumn(0).setResizable(false);
+            cajeroTable.getColumnModel().getColumn(1).setResizable(false);
+            cajeroTable.getColumnModel().getColumn(2).setResizable(false);
+            cajeroTable.getColumnModel().getColumn(3).setResizable(false);
         }
 
         getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 170, 1130, 390));
@@ -152,7 +158,7 @@ public class Reportes extends javax.swing.JFrame {
                 jButtonDesplegarCajerosActionPerformed(evt);
             }
         });
-        getContentPane().add(jButtonDesplegarCajeros, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 50, -1, -1));
+        getContentPane().add(jButtonDesplegarCajeros, new org.netbeans.lib.awtextra.AbsoluteConstraints(1060, 130, -1, -1));
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel6.setText("Buscar por código de factura:");
@@ -428,6 +434,7 @@ public class Reportes extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable cajeroTable;
     private javax.swing.JTextField codfactInput;
     private javax.swing.JMenu empleadosMenu;
     private javax.swing.JTextField fechaBusInput;
@@ -447,7 +454,6 @@ public class Reportes extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JPopupMenu.Separator jSeparator1;
-    private javax.swing.JTable jTable1;
     private javax.swing.JButton reporteCajeroBtn;
     private javax.swing.JButton reporteGeneralBtn;
     private javax.swing.JMenuItem reporteVentaField;
