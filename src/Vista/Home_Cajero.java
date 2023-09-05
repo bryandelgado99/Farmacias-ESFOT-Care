ea966e35502fc23cd7832dcdc1c4de2a0c5d3ab1
DavidPK8 <alejodavicho8@gmail.com>
2023-09-05 08:56:28 -0500

    Cambios
    - Limpieza de tabla y datos de cliente
    - Cambio de NumFactura una vez finalizada la compra
    - Almacenamiento de compra en la base de datos

--- src/Vista/Home_Cajero.java
+++ src/Vista/Home_Cajero.java
@@ -682,7 +682,10 @@ public class Home_Cajero extends javax.swing.JFrame {
             clienteDireccion.setText("");
             cliTelef.setText("");
             cliMail.setText("");
-
+            String numId = numFactLabel.getText();
+            int numeroId = Integer.parseInt(numId) + 1;
+            String numeroIdT = String.valueOf(numeroId);
+            numFactLabel.setText(numeroIdT);
             conn.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
